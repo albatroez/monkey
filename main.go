fc1baded403b6a3147fe1e6215cb1327f1d0b997
Jakub Zielinski <kuba.s.zielinski@gmail.com>
2024-04-06 12:17:45 +0200

    parsing let statements

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 )
 
 func main() {
-	user, err := user.Current()
+	usr, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", usr.Username)
 	fmt.Println("Feel free to type in commands")
 	repl.Start(os.Stdin, os.Stdout)
 }
