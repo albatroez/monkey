e871bfb34ece7b0bcb8870a5456bde8f3af73c91
Jakub Zielinski <kuba.s.zielinski@gmail.com>
2025-02-13 15:00:51 +0100

    2.9 - Read-Parse-Print-Loop

--- main.go
+++ main.go
@@ -7,11 +7,25 @@ import (
 	"os/user"
 )
 
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
 func main() {
 	usr, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf(MONKEY_FACE)
 	fmt.Printf("Hello %s! This is the Monkey programming language!\n", usr.Username)
 	fmt.Println("Feel free to type in commands")
 	repl.Start(os.Stdin, os.Stdout)
